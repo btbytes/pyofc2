bccb301d0eebae2110f7ee35e8ba37a35030766c
Pradeep Gowda <pradeep@btbytes.com>
2009-01-03 03:38:40 -0500

    new demo files for area, bar, scatter charts

--- examples.py
+++ examples.py
@@ -11,13 +11,18 @@ from pyofc2 import *
 import time   
 import math 
 import random
-import inspect
+import inspect  
+import pprint
 from jinja import Environment, FileSystemLoader
 import markdown2 #http://code.google.com/p/python-markdown2/
 
+#utilitarian objects
+months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct',
+    'Nov', 'Dec']
+    
 def test_line():      
     '''  
-    Line/Line
+    Line Charts/Line
     Simple Line Graph
     '''
     t = title(text=time.strftime('%a %Y %b %d'))
@@ -37,8 +42,8 @@ def frange(a,b,incr):
     
 def test_line_dot():
     '''
-    Line/Line Dot
-    Examples of Dotted Line Chart
+    Line Charts/Line Dot
+    Example of Dotted Line Chart
     '''
     t = title(text=time.strftime('%a %Y %b %d'))     
     l1 = line_dot()
@@ -73,8 +78,8 @@ def test_line_dot():
 
 def test_line_hollow():
     '''
-    Line/Line Hollow 
-    Examples of Hollow Line Charts  
+    Line Charts/Line Hollow 
+    Example of Hollow Line Charts  
     '''
     t = title(text=time.strftime('%a %Y %b %d'))     
     l1 = line_hollow()
@@ -108,8 +113,8 @@ def test_line_hollow():
 
 def test_manylines():
     '''
-    Line/Many Lines    
-    Examples of Multiple Line Charts
+    Line Charts/Many Lines    
+    Example of Multiple Line Charts
     '''       
     l1 = line_dot()
     l1.width = 4
@@ -144,8 +149,194 @@ def test_manylines():
     chart.add_element(l2)    
     chart.add_element(l3)
     chart.y_axis = y
-    return chart      
+    return chart       
+    
+def test_bar():
+    '''
+    Bar Charts/Bar Chart
+    Make a Bar Chart
+    To add more than one set of bars to a bar chart, see [Bar Chart 2](bar_2.html)
+    '''                                                                                 
+    t = title(text=time.strftime('%a %Y %b %d'))
+    b = bar()
+    b.values = range(9,0,-1)
+    chart = open_flash_chart()
+    chart.title = t
+    chart.add_element(b)
+    return chart
+     
+def test_bar_2():
+    '''
+    Bar Charts/Bar Chart 2
+    Add more than one bar to the chart  
+    '''                                                                                 
+    t = title(text=time.strftime('%a %Y %b %d'))
+    b1 = bar()
+    b1.values = range(9,0,-1)
+    b2 = bar()
+    b2.values = [random.randint(0,9) for i in range(9)]
+    b2.colour = '#56acde'
+    chart = open_flash_chart()
+    chart.title = t    
+    chart.add_element(b1)
+    chart.add_element(b2)
+    return chart
+    
 
+def test_bar_filled():
+    '''
+    Bar Filled Charts/Bar Filled Chart
+    Set the outline colour
+    '''                                                                                 
+    t = title(text=time.strftime('%a %Y %b %d'))
+    b1 = bar_filled(colour='#E2D66A')
+    b1.values = range(9,0,-1)
+    b1.outline_colour = '#577261'
+    chart = open_flash_chart(bg_colour="#FFFFFF")
+    chart.title = t    
+    chart.add_element(b1)
+    return chart
+
+def test_horizontal_bar():
+    '''
+    Horizontal Bar Charts/Horizontal Bar Chart
+    Make a horizontal bar chart
+    '''                        
+    t = title(text="Our New House Schedule")
+    hb = hbar(colour="#86BBEF")
+    hb.tooltip = ("Months: #val#")
+    values = [ hbar_value(left=0, right=4), 
+        hbar_value(left=4, right=8)]
+    h3 = hbar_value(left=8, right=11)
+    h3.tip = "#left# to #right#<br>%s to %s (#val# months)" % (months[8], months[11]) 
+    values.append(h3)
+    hb.values = values
+    chart = open_flash_chart()
+    chart.title = t    
+    chart.add_element(hb)    
+    x = x_axis()
+    x.offset = False
+    x.labels = labels(labels=months)
+    y = y_axis()
+    y.offset = 1
+    y.labels = ["Make garden look sexy","Paint house","Move into house"]
+    chart.x_axis = x
+    chart.y_axis = y
+    return chart
+    
+def scurve():
+    i = 0.0
+    data = []
+    while i <=6.2:
+        data.append(math.sin(i)*1.9)
+        i += 0.2
+    return data
+    
+def test_area_line():
+    '''
+    Area Charts/Area Line Chart
+    Make an area chart.
+    
+    See also [Area Hollow](area_hollow.html)
+    '''                                     
+    t = title(text="Area Chart")
+    a = area_line()
+    a.width = 2
+    a.dot_size = 4
+    a.halo_size = 1
+    a.colour = '#C4B86A'
+    a.fill_colour = '#C4B8AA'
+    a.fill_alpha = 0.7 
+    a.values = scurve()
+    chart = open_flash_chart()
+    chart.title = t
+    chart.add_element(a)
+    y  = y_axis()
+    y.min, y.max, y.steps = -2, 2, 2
+    y.labels = None
+    y.offset = False
+    
+    x = x_axis()
+    x.labels = scurve()
+    x.steps = 2
+    xlbls = x_axis_labels(steps=4, rotate="vertical")
+    x.labels = xlbls
+    
+    chart.y_axis = y
+    chart.x_axis = x
+    return chart
+    
+def test_area_hollow():
+    '''
+    Area Charts/Area Hollow Chart
+    Make an area chart.
+
+    See also [Area Line](area_line.html)
+    '''                                     
+    t = title(text="Area Hollow Chart")
+    a = area_hollow()
+    a.width = 2
+    a.dot_size = 4
+    a.halo_size = 1
+    a.colour = '#C4B86A'
+    a.fill_colour = '#C4B8AA'
+    a.fill_alpha = 0.7 
+    a.values = scurve()
+    chart = open_flash_chart()
+    chart.title = t
+    chart.add_element(a)
+    y  = y_axis()
+    y.min, y.max, y.steps = -2, 2, 2
+    y.labels = None
+    y.offset = False
+
+    x = x_axis()
+    x.labels = scurve()
+    x.steps = 2
+    xlbls = x_axis_labels(steps=4, rotate="vertical")
+    x.labels = xlbls
+
+    chart.y_axis = y
+    chart.x_axis = x
+    return chart        
+    
+def scircle(): 
+    data = []
+    for i in range(0,360,5):    
+        sv = scatter_value(
+            x='%.2f' % math.sin(math.radians(i)),
+            y='%.2f' % math.cos(math.radians(i))
+        )
+        data.append(sv)
+    return data
+    
+    
+def test_scatter_chart():
+    '''
+    Scatter Charts/Scatter Chart
+    Make a scatter chart 
+    
+    *Note:* The above plot should look circular but looks elliptical instead
+    because the y-axis is scaled w.r.t x-axis (780:300).
+    
+    '''                 
+    chart = open_flash_chart()
+    chart.title = title(text='Scatter Chart')
+    s1 = scatter(colour="#FFD600", dot_size=10)
+    s1.values = [scatter_value(x=0, y=0)]
+    chart.add_element(s1)
+    s2 = scatter(colour="#D600FF", dot_size=3)
+    s2.values = scircle()
+    chart.add_element(s2)
+    x = x_axis()
+    y = y_axis()
+    x.min, x.max, x.steps = -2, 2, 1
+    y.min, y.max, y.steps = -2, 2, 1
+    chart.x_axis = x
+    chart.y_axis = y
+    return chart
+        
+        
 def test_x_axis_labels_1():
     '''
     X Axis Labels/X Axis Labels 1
@@ -205,6 +396,9 @@ def _test():
     test_line_hollow()
     test_manylines()
     test_x_axis_labels_1()
+    test_x_axis_labels_1()
+    test_bar()
+    
 	
 ## The following functions are used to auto-generate demo files from this module.
 def listfunc():
@@ -238,7 +432,6 @@ def build_examples():
     for f in funcs:
         fdoc = f.__doc__.split('\n')
         cat, title = fdoc[1].strip().split('/')
-        print  cat, categories
         doc = markdown2.markdown('\n'.join([l.strip() for l in fdoc[2:]]))
         name = f.__name__.split('test_')[1]
         json = ''
@@ -279,14 +472,14 @@ def build_examples():
         outf.write(e['json'])
         outf.close()     
 
-    sidebar = '<ul>%s</ul>'
+    sidebar = '<dl>%s</dl>'
     links = []
     for cat in categories:
-        links.append('<ul><h4>%s</h4>' % cat)
+        links.append('<dt class="el2">%s</dt><dd>' % cat)
         for e in examples: #inefficient.. much
             if cat == e['cat']:
                 links.append('<li><a href="%s">%s</a></li>' %(e['name']+'.html', e['title']))      
-        links.append('</ul>')
+        links.append('</dd>')
     sidebar = sidebar % '\n'.join(links)
         
     #generate demo-html files
