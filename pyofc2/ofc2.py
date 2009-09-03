ff8e16ba0e512e42856e7b1f91514e1ed3c09ff4
Josh Borke <joshborke+github@gmail.com>
2009-09-03 15:24:36 -0400

    remove trailing whitespaces because they suck

--- pyofc2/ofc2.py
+++ pyofc2/ofc2.py
@@ -4,7 +4,7 @@
 ofc2.py
 
 Created by Pradeep Gowda on 2009-01-01.
-Copyright (c) 2009 Pradeep Gowda. 
+Copyright (c) 2009 Pradeep Gowda.
 Permission is hereby granted, free of charge, to any person
 obtaining a copy of this software and associated documentation
 files (the "Software"), to deal in the Software without
@@ -28,28 +28,28 @@ OTHER DEALINGS IN THE SOFTWARE.
 """
 
 import cjson
-                 
-class OFCBase(dict): 
+
+class OFCBase(dict):
     type = None
     replace = {
         'font_size':'font-size', 'fontsize': 'font-size',
         'color':'colour', 'bg_color':'bg_colour', 'bgcolor':'bg_colour',
         'dot_size': 'dot-size', 'dotsize':'dot-size', 'grid_colour': 'grid-colour',
-        'grid_color': 'grid-colour', 'tick_height': 'tick-height', 
+        'grid_color': 'grid-colour', 'tick_height': 'tick-height',
         'on_click':'on-click', 'outline_color':'outline-colour',
         'outline_colour':'outline-colour', 'fill_color':'fill',
         'fill_colour':'fill', 'fill_alpha':'fill-alpha',
         'halo_size':'halo-size', 'halosize':'halo-size',
         'proximity': 'mouse', 'spoke_labels':'spoke-labels'
     }
-     
-    def __setattr__(self, k, w):       
+
+    def __setattr__(self, k, w):
         if k in self.acceptable:
             if k in self.replace.keys():
                 k = self.replace[k]
             self[k] = w
             self.__dict__.update({k:w})
-     
+
 
 def ofc_init(self, **kw):
     for k,w in kw.items():
@@ -59,12 +59,12 @@ def ofc_init(self, **kw):
             self[k] = w
             self.__dict__.update({k:w})
 
-    
+
 def ofc_factory(classname, acceptable):
        klass = type(classname, (OFCBase, ), {'acceptable':acceptable})
        setattr(klass, '__init__', ofc_init )
        return klass
-            
+
 title = ofc_factory('title', ['text','style'])
 class x_legend(title): pass
 class y_legend(title): pass
@@ -72,16 +72,16 @@ class y_legend(title): pass
 labels = ofc_factory('labels', ['labels'])
 x_axis_label = ofc_factory('x_axis_label', ['text', 'steps', 'color', 'colour', 'size', 'visible', 'rotate' ])
 x_axis_labels = ofc_factory('x_axis_labels', ['labels', 'rotate', 'steps'])
-radar_axis_labels = ofc_factory('radar_axis_labels', ['labels'])  
+radar_axis_labels = ofc_factory('radar_axis_labels', ['labels'])
 radar_spoke_labels = ofc_factory('radar_spoke_labels',['labels'])
 shapefactory = ofc_factory('_shape', ['type','colour', 'color', 'values'])
 shape = lambda **kw: shapefactory(type='shape', **kw)
 shape_value = ofc_factory('shape_value', ['x', 'y'])
 
-axis =  ofc_factory('axis', ['stroke', 'tick_height', 'colour', 
+axis =  ofc_factory('axis', ['stroke', 'tick_height', 'colour',
     'grid_colour', 'steps', 'min', 'max', 'labels', 'offset', 'radar',
     'spoke_labels'
-    ])                                          
+    ])
 class x_axis(axis): pass
 class y_axis(axis): pass
 class y_axis_right(axis): pass
@@ -91,10 +91,10 @@ tooltip = ofc_factory('tooltip', ['shadow', 'stroke', 'colour', 'bg_colour', 'ti
 
 element = ofc_factory('element', ['type','alpha', 'colour', 'color', 'text', 'fontsize', 'values'])
 
-linefactory = ofc_factory('_line', ['type','alpha', 'colour','color', 'text', 
+linefactory = ofc_factory('_line', ['type','alpha', 'colour','color', 'text',
     'fontsize', 'font_size', 'values', 'halo_size', 'width', 'dot_size', 'on_click', 'tip',
     'loop'])
-line = lambda **kw: linefactory(type='line',**kw)     
+line = lambda **kw: linefactory(type='line',**kw)
 line_dot = lambda **kw: linefactory(type='line_dot', **kw)
 line_hollow = lambda **kw: linefactory(type='line_hollow', **kw)
 
@@ -103,18 +103,18 @@ dot_value = ofc_factory('value', ['value', 'colour', 'color', 'tip'])
 barfactory = ofc_factory('_bar', ['type', 'values', 'alpha', 'color', 'colour', 'key'])
 bar = lambda **kw: barfactory(type='bar',**kw)
 
-barfilledfactory = ofc_factory('_bar', ['type', 'values', 'alpha', 'color', 
+barfilledfactory = ofc_factory('_bar', ['type', 'values', 'alpha', 'color',
     'colour', 'key', 'outline_colour', 'outline_color'])
 bar_filled = lambda **kw: barfilledfactory(type='bar_filled',**kw)
 
 hbarfactory = ofc_factory('_hbar', ['type', 'values', 'color', 'colour', 'tooltip', 'tip'])
 hbar = lambda **kw: hbarfactory(type='hbar', **kw)
 hbar_value = ofc_factory('hbar_factory', ['left', 'right', 'tip'])
-bar_stack = lambda **kw: element(type='barstack',**kw)  
+bar_stack = lambda **kw: element(type='barstack',**kw)
 
-area_linefactory = ofc_factory('_area_line', ['type', 'values', 'color', 'colour', 
-    'tooltip', 'width', 'dot_size', 'dotsize', 'halo_size', 'halosize' 'key', 'fill_colour', 
-    'fill_color', 'fill_alpha', 'loop']) 
+area_linefactory = ofc_factory('_area_line', ['type', 'values', 'color', 'colour',
+    'tooltip', 'width', 'dot_size', 'dotsize', 'halo_size', 'halosize' 'key', 'fill_colour',
+    'fill_color', 'fill_alpha', 'loop'])
 area_line = lambda **kw: area_linefactory(type='area_line', **kw)
 area_hollow = lambda **kw: area_linefactory(type='area_hollow', **kw)
 
@@ -125,7 +125,7 @@ scatter_line = lambda **kw: scatterfactory(type='scatter_line', **kw)
 
 
 #TODO: derive open_flash_chart class from OFCBase . use ofc_factory
-class open_flash_chart(OFCBase):               
+class open_flash_chart(OFCBase):
     typetable = {
     'title': title,
     'x_legend':x_legend,
@@ -151,6 +151,6 @@ class open_flash_chart(OFCBase):
 
     def __str__(self):
         return cjson.encode(self)
-        
+
     def render(self):
         return cjson.encode(self)
